969b38e81c374adf0a7392bda17a9652fd175853
Britta Haupt <britta.haupt@dermalog.com>
2024-12-14 21:01:32 +0100

    url query params

--- fishes.js
+++ fishes.js
@@ -6,18 +6,24 @@ fetch(source)
     var fish = data.fish;
     // see that all is there just in case
     console.log(fish);
+    // create fishes table like in Critterpedia
     var fishList = document.createElement("table");
     fishList.setAttribute("id", "fishList");
     document.getElementById("fishes").appendChild(fishList);
 
     var tbody = fishList.createTBody();
+    // 5 rows
     for (var i = 1; i <= 5; i++) {
+      // add in last position, JS is stupid...
       var row = tbody.insertRow(-1);
+      // 15 columns
       for (var j = 0; j <= 75; j = j + 5) {
         {
+          // all fish
           var index = i + j;
           var cell = row.insertCell(j / 5);
-          var html = '<a href="fish.html?index=' + index + '" target="_self">';
+          // picture as link
+          var html = '<a href="fish.html?index=' + index + '">';
           html +=
             '<img src="./images/fish/' +
             index +
@@ -27,7 +33,6 @@ fetch(source)
           html += "</a>";
           html += "";
           cell.innerHTML = html;
-          //cell.innerHTML =  "<img src=\"./images/fish/" + index + ".jpeg\" title= \"" + fish[index].name + "\" alt=\"not found yet\">";
         }
       }
     }
