c630d5552845aa7663c33244a0cc7b30c44922fb
Britta Haupt <britta.haupt@dermalog.com>
2024-12-14 21:04:41 +0100

    url query params

--- fish.js
+++ fish.js
@@ -2,4 +2,13 @@ const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 const index = urlParams.get("index");
-console.log(index);
+
+// Fetch data from JSON file for convenience
+var source = "data/fish.json";
+fetch(source)
+  .then((response) => response.json())
+  .then((data) => {
+    var fishes = data.fish;
+    fish = fishes[index];
+    console.log(fish);
+    });
